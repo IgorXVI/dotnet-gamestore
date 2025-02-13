783583e1e0688b5db2ea2aa8f54af7dd521b8477
Igor Tomelero de Almeida <inazumaseleven04@gmail.com>
2025-02-13 02:12:01 -0300

    retornando dtos nas rotas

--- Endpoints/GamesEndpoints.cs
+++ Endpoints/GamesEndpoints.cs
@@ -2,6 +2,7 @@ using System;
 using GameStore.Data;
 using GameStore.Dtos;
 using GameStore.Entities;
+using GameStore.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Endpoints;
@@ -15,39 +16,31 @@ public static class GamesEndpoints
 
         group.MapGet("/", (GameStoreContext dbContext) =>
         {
-            var games = dbContext.Games.ToList();
+            var games = dbContext.Games.Include(x => x.Genre).ToArray().Select(x => x.ToDto());
             return Results.Ok(games);
         });
 
         group.MapPost("/", (CreateGameDTO newGame, GameStoreContext dbContext) =>
         {
-            Game game = new()
-            {
-                Name = newGame.Name,
-                Genre = dbContext.Genres.Find(newGame.GenreId),
-                GenreId = newGame.GenreId,
-                Price = newGame.Price,
-                ReleaseDate = newGame.ReleaseDate,
-            };
+            Game game = newGame.ToEntity();
+            game.Genre = dbContext.Genres.Find(newGame.GenreId);
 
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
 
-            return Results.CreatedAtRoute(GET_GAME_ROUTE_NAME, new { id = game.Id }, game);
+            return Results.CreatedAtRoute(GET_GAME_ROUTE_NAME, new { id = game.Id }, game.ToDto());
         });
 
         group.MapGet("/{id}", (int id, GameStoreContext dbContext) =>
         {
-            var oldGame = dbContext.Games
-                .Include(x => x.Genre)
-                .FirstOrDefault(x => x.Id == id);
+            var oldGame = dbContext.Games.Include(x => x.Genre).FirstOrDefault(x => x.Id == id);
 
             if (oldGame == null)
             {
                 return Results.NotFound();
             }
 
-            return Results.Ok(oldGame);
+            return Results.Ok(oldGame.ToDto());
         }).WithName(GET_GAME_ROUTE_NAME);
 
         group.MapPut("/{id}", (int id, UpdateGameDTO newGame, GameStoreContext dbContext) =>
@@ -67,7 +60,7 @@ public static class GamesEndpoints
             dbContext.Games.Update(oldGame);
             dbContext.SaveChanges();
 
-            return Results.Ok(oldGame);
+            return Results.Ok();
         });
 
         group.MapDelete("/{id}", (int id, GameStoreContext dbContext) =>
